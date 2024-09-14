d8dd16ca4fe754675a2be1b0b1f7d6407848cd50
CristianPC935 <cristianpalacios935@gmail.com>
2024-09-13 19:58:34 -0600

    primer commit

---
+++ back-end/control-notas-asistencia/src/test/java/com/proyectograduacion/controlasistencianotas/ControlDeNotasYAsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectograduacion.controlasistencianotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeNotasYAsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
