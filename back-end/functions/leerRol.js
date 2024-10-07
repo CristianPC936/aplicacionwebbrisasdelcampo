6a26fb096ce1205059cc0d2301efee67409210e2
CristianPC935 <cristianpalacios935@gmail.com>
2024-10-06 23:55:35 -0600

    funciones usuario funcionando, V 0.0.1

---
+++ back-end/functions/leerRol.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql');
+
+// Configurar la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: 'serveo.net',
+  user: 'root',
+  password: '',
+  database: 'pruebaproyecto',
+  port: 13306
+});
+
+exports.handler = async (event, context) => {
+  // Consulta SQL para obtener los roles
+  const sql = 'SELECT idRol, nombreRol FROM Rol';
+
+  // Retornar una promesa para manejar la consulta de manera asíncrona
+  return new Promise((resolve, reject) => {
+    connection.query(sql, (error, results) => {
+      if (error) {
+        reject({
+          statusCode: 500,
+          headers: {
+            'Access-Control-Allow-Origin': '*',  // Permitir solicitudes desde cualquier origen
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',  // Métodos permitidos
+            'Access-Control-Allow-Headers': 'Content-Type'  // Encabezados permitidos
+          },
+          body: JSON.stringify({ error: 'Error al obtener los roles: ' + error })
+        });
+      } else {
+        resolve({
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*',  // Permitir solicitudes desde cualquier origen
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',  // Métodos permitidos
+            'Access-Control-Allow-Headers': 'Content-Type'  // Encabezados permitidos
+          },
+          body: JSON.stringify(results)  // Devolver los resultados en formato JSON
+        });
+      }
+    });
+  });
+};
